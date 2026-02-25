151e2b0dbfcd4b3660fee343d2e9413c2109f5b5
Zarija Aleksoski <160492258+Aleksoski1983@users.noreply.github.com>
2026-02-25 13:14:40 +0100

    Delete IIoT_Gateway_on_Raspberry_Pi-Upgrade-functionalities directory

--- IIoT_Gateway_on_Raspberry_Pi-Upgrade-functionalities/app/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API Blueprint Registration
-"""
-
-from flask import Blueprint
-
-api_bp = Blueprint('api', __name__)
-
-from app.api import opcua_routes, mqtt_routes, data_routes
--- IIoT_Gateway_on_Raspberry_Pi-Upgrade-functionalities/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Services Module
-Background services for MQTT, OPC UA, and database operations
-"""
